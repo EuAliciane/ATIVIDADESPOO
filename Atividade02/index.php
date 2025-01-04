bd3caadd9dfb9b2eaf8fc58c5c6fcbe02fe98341
EuAliciane <98620470+EuAliciane@users.noreply.github.com>
2025-01-04 19:53:34 -0300

    Add files via upload

---
+++ Atividade02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+        <meta charset="UTF-8">
+        <title>Atividade aula</title>
+    </head>
+    <body>
+<?php
+require_once 'Onibus.php';
+$o1= new Onibus;
+$o1->origem = "Terminal do Siqueira";
+$o1->destino = "Terminal Santa Felicidade";
+$o1->linha = 250;
+$o1->embarque = false;
+$o1->pegaronibus();
+print_r($o1);
+echo "</br>";
+$o2= new Onibus;
+$o2->origem = "Terminal Santa Felicidade";
+$o2->destino = "Terminal do Siqueira";
+$o2->linha = 250;
+$o2->embarque = true;
+$o2->pegaronibus();
+print_r($o2);
+ ?>
+    </body>
+</html>
