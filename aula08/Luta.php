12a77d1d08b4c92a53693e055057da4ec1df64b1
EuAliciane <98620470+EuAliciane@users.noreply.github.com>
2025-01-29 12:31:51 -0300

    Add files via upload
    Continuação do exercício de UltraEmojiCombat, neste exercício os lutadores poderão competir.

---
+++ aula08/Luta.php
@@ -0,0 +1,74 @@
+<?php
+require_once 'Lutador.php';
+class Luta {
+    //Atributos
+    private $desafiado;
+    private $desafiante;
+    private $rounds;
+    private $aprovada;
+    //Métodos
+    public function marcarLuta($l1, $l2) {
+            if($l1->getCategoria() === $l2->getCategoria() 
+                    && $l1 != $l2) {
+                $this->aprovada = true;
+                $this->desafiado = $l1;
+                $this->desafiante =$l2;
+            }else{
+                $this->aprovada = false;
+                $this->desadiado = null;
+                $this->desafiante = null;
+            }
+    }
+    public function lutar() {
+        if($this->aprovada){
+            $this->desafiado->apresentar();
+            $this->desafiante->apresentar();
+            $vencedor =rand(0,2);
+            switch($vencedor){
+                case 0: //empate
+                    echo "Empatou";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                case 1: //ganhou desafiado
+                    echo "</br>{$this->desafiado->getNome()} venceu";
+                    $this->desafiado->ganharLuta();
+                    $this->desafiante->perderLuta();
+                    break;
+                case 2: //ganhou desafiante
+                    echo "</br>{$this->desafiante->getNome()}";
+                    $this->desafiante->ganharLuta();
+                    $this->desafiado->perderLuta();
+                    break;
+            }
+        }else {
+            echo "</br>--------------------";
+            echo "</br>Luta não pode acontecer";
+            
+        }
+    }
+    function getDesafiado() {
+        return $this->desafiado;
+    }
+    function getDesafiante() {
+        return $this->desafiante;
+    }
+    function getRounds() {
+        return $this->rounds;
+    }
+    function getAprovada() {
+        return $this->aprovada;
+    }
+    function setDesafiado($desafiado) {
+        $this->desafiado = $desafiado;
+    }
+    function setDesafiante($desafiante) {
+        $this->desafiante = $desafiante;
+    }
+    function setRound($rounds) {
+        $this->rounds = $rounds;
+    }
+    function setAprovada($aprovada) {
+        $this->aprovada = $aprovada;
+    }
+}
